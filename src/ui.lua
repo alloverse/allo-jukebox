999274a1d171bb0c51c77867d62196bfb0cbc09d
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-04-18 23:54:32 +0200

    maybe better API?

--- src/ui.lua
+++ src/ui.lua
@@ -14,6 +14,25 @@ end
 
 class.Button(View)
 
+class.Pose()
+-- Pose(): create zero pose
+-- Pose(transform): create pose from transform
+-- Pose(x, y, z): create positioned pose
+-- Pose(a, x, y, z): create rotated pose
+function Pose:_init(a, b, c, d)
+    if b == nil then
+        self.transform = a or mat4.identity()
+    elseif d == nil then
+        self.transform = mat4.translate(mat4.identity(), mat4.identity(), vec3.new(a, b, c))
+    else
+        self.transform = mat4.rotate(mat4.identity(), mat4.identity(), a, vec3.new(b, c, d))
+    end
+end
+
+function Pose:rotate(angle, x, y, z)
+    mat4.rotate(self.transform, self.transform, angle, vec3(x, y, z))
+end
+
 class.Size()
 function Size:_init(width, height)
     self.width = width
@@ -21,17 +40,23 @@ function Size:_init(width, height)
 end
 
 class.Frame()
-function Frame:_init(pose, size)
-    self.pose = pose
-    self.size = size
+-- Frame(pose, size)
+-- Frame(x, y, z, w, h)
+function Frame:_init(a, b, z, w, h)
+    if type(pose) == "table" then
+        self.pose = a
+        self.size = b
+    else
+        self.pose = Pose(a, b, z)
+        self.size = Size(w, h)
+    end
 end
+
 
 return {
     View = View,
     Button = Button,
     Frame = Frame,
-    Size = Size,
-    position = function(x, y, z)
-        return mat4.translate(mat4.identity(), mat4.identity(), vec3.new(x, y, z))
-    end
+    Pose = Pose,
+    Size = Size
 }
