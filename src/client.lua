6ed6a08b2686c90fa5a8403e0d6fa2d55a143d68
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-04-18 22:39:17 +0200

    update client to match new allonet API

--- src/client.lua
+++ src/client.lua
@@ -5,7 +5,25 @@ local Client_mt = {__index = Client}
 function Client.connect(url, name)
     local self = {}
     setmetatable(self, Client_mt)
-    self.client = allonet.connect(
+    self.client = allonet.create()
+
+    self.client:set_disconnected_callback(function()
+        print("Lost connection :(")
+        exit()
+    end)
+    self.client:set_interaction_callback(function(inter)
+        local body = json.decode(inter.body)
+        if body[1] == "announce" then
+            self.avatar_id = body[2]
+            print("Determined avatar ID: " .. self.avatar_id)
+        end
+    end)
+    self.client:set_state_callback(function(state)
+    
+    end)
+    self.avatar_id = ""
+
+    self.client:connect(
         url,
         json.encode({display_name = name}),
         json.encode({
@@ -14,25 +32,6 @@ function Client.connect(url, name)
             }
         })
     )
-    self.avatar_id = ""
-
-    self.client:set_disconnected_callback(function()
-        print("Lost connection :(")
-        exit()
-    end)
-    
-    self.client:set_interaction_callback(function(inter)
-        local body = json.decode(inter.body)
-        if body[1] == "announce" then
-            self.avatar_id = body[2]
-            print("Determined avatar ID: " .. self.avatar_id)
-        end
-    end)
-    
-    self.client:set_state_callback(function(state)
-    
-    end)
-    
     return self
 end
 
